56d80074cbce4759bd408bb8ac34af3f5fe3a124
Ibrahim Salah <ibrahimsalah093@gmail.com>
2025-05-06 01:24:29 +0300

    Update iOS, Andriod framework sources

--- scripts/setup_ios.sh
+++ scripts/setup_ios.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # iOS 框架下载解压
-IOS_URL="https://github.com/hellohejinyu/ffmpeg_kit_flutter_full_gpl/releases/download/v5.1.LTS/ffmpeg-kit-full-gpl-5.1.LTS-ios-framework.zip"
+IOS_URL="https://github.com/ibrahimsalah1/ffmpeg_kit_flutter_full_gpl/releases/download/v5.1.LTS/ffmpeg-kit-full-gpl-5.1.LTS-ios-framework.zip"
 mkdir -p Frameworks
 curl -L $IOS_URL -o frameworks.zip
 unzip -o frameworks.zip -d Frameworks
